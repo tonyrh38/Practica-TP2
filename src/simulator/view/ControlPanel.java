ffd1d146f4eb611c3dca9a947ae75e0c7162dd00
tonyrh38 <antrodrigh@gmail.com>
2020-04-23 14:06:38 +0200

    Se viene lo fuerte. Comenzamos con barra superior

---
+++ src/simulator/view/ControlPanel.java
@@ -0,0 +1,60 @@
+package simulator.view;
+
+import java.util.List;
+
+import javax.swing.JPanel;
+
+import simulator.control.Controller;
+import simulator.model.Event;
+import simulator.model.RoadMap;
+import simulator.model.TrafficSimObserver;
+
+public class ControlPanel extends JPanel implements TrafficSimObserver {
+
+	private static final long serialVersionUID = -4423199850333010661L;
+	
+	private Controller _controller;
+	
+	public ControlPanel(Controller controller) {
+		// TODO: completar constructor
+		_controller = controller;
+	}
+	
+	
+	@Override
+	public void onAdvanceStart(RoadMap map, List<Event> events, int time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onAdvanceEnd(RoadMap map, List<Event> events, int time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onEventAdded(RoadMap map, List<Event> events, Event e, int time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onReset(RoadMap map, List<Event> events, int time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onRegister(RoadMap map, List<Event> events, int time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onError(String err) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
